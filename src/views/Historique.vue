f9416892b10403b40ef1b568f86d186f607c6caf
maxime mansuy <mmansuy51@gmail.com>
2025-04-05 17:30:37 +0200

    feat: update

--- src/views/Historique.vue
+++ src/views/Historique.vue
@@ -1,11 +1,13 @@
 <script setup>
-import {ref, onMounted} from "vue";
+import {ref, onMounted, watch} from "vue";
 import {client} from "../../sanity.js";
 
 const user = ref([]);
 const clientNom = ref("");
 const listeFactures = ref([]);
+const listeFiltered = ref([]);
 const watchPdf = ref("")
+const filtrerApply = ref("tout")
 const objectToUpdate = ref("")
 
 const fetchProfessionnels = async () => {
@@ -16,11 +18,11 @@ const fetchProfessionnels = async () => {
 
 onMounted(async () => {
   await fetchProfessionnels()
+  await rechercher()
 })
 
 const rechercher = async () => {
-  if (clientNom.value) {
-    const query = `*[_type == "facture" && client == "${clientNom.value}" && professionnel._ref == "${user.value[0]._id}"]{
+  const query = `*[_type == "facture" && professionnel._ref == "${user.value[0]._id}"]{
   _id,
   title,
   date,
@@ -36,7 +38,7 @@ const rechercher = async () => {
     title
   }
 }`;
-    const query2 = `*[_type == "devis" && client == "${clientNom.value}" && professionnel._ref == "${user.value[0]._id}"]{
+  const query2 = `*[_type == "devis" && professionnel._ref == "${user.value[0]._id}"]{
   _id,
   title,
   date,
@@ -52,16 +54,28 @@ const rechercher = async () => {
     title
   }
 }`;
-    const [result1, result2] = await Promise.all([client.fetch(query), client.fetch(query2)]);
-    const list = [...result1, ...result2];
+  const [result1, result2] = await Promise.all([client.fetch(query), client.fetch(query2)]);
+  const list = [...result1, ...result2];
 
-    listeFactures.value = list;
-  }
+  list.sort((a, b) => {
+    // Convertir les valeurs en minuscules pour un tri insensible à la casse
+    const clientA = a.client.toLowerCase();
+    const clientB = b.client.toLowerCase();
+
+    // Comparer les deux valeurs
+    if (clientA < clientB) return -1;
+    if (clientA > clientB) return 1;
+    return 0;
+  });
+
+  listeFactures.value = list;
+  listeFiltered.value = list;
 };
 
 const deletePdf = async (id) => {
   await client.delete(id);
   alert("Suppression réalisée avec succès !");
+  await rechercher();
 }
 const getUrl = (file) => {
   if(file.title.includes("Fact")){
@@ -70,26 +84,36 @@ const getUrl = (file) => {
   }
   objectToUpdate.value = `/devis?id=${file._id}`;
 }
+
+watch(filtrerApply, ()=>{
+  if(filtrerApply.value === 'devis'){
+    listeFiltered.value = listeFactures.value.filter((list) => list.title.includes("Devis"));
+  }else if(filtrerApply.value === 'fact'){
+    listeFiltered.value = listeFactures.value.filter((list) => list.title.includes("Fact"));
+  }else{
+    listeFiltered.value = listeFactures.value;
+  }
+})
 </script>
 
 <template>
   <div class="principal-div">
     <div class="input-div">
-      <div>
-        <label >Rechercher par nom de client : </label>
-        <input v-model="clientNom" class="border p-2 rounded w-full"/>
-        <button @click="rechercher()" class="ml-6 text-red-500">Rechercher</button>
-      </div>
-      <table class="min-w-full table-auto border-collapse" v-if="listeFactures.length">
+      <button @click="filtrerApply ='tout'" class="ml-6 text-red-500">Tout</button>
+      <button @click="filtrerApply ='devis'" class="ml-6 text-red-500">Que Devis</button>
+      <button @click="filtrerApply ='fact'" class="ml-6 text-red-500">Que factures</button>
+      <table class="min-w-full table-auto border-collapse" v-if="listeFiltered.length">
         <thead>
         <tr class="bg-gray-100 text-left">
+          <th class="px-4 py-2 font-semibold text-gray-700">Nom Client</th>
           <th class="px-4 py-2 font-semibold text-gray-700">Nom Facture</th>
           <th class="px-4 py-2 font-semibold text-gray-700">Date de Création</th>
           <th class="px-4 py-2 font-semibold text-gray-700">PDF</th>
         </tr>
         </thead>
         <tbody>
-        <tr class="border-t" v-for="list of listeFactures" :key="list._id">
+        <tr class="border-t" v-for="list of listeFiltered" :key="list._id">
+          <td class="px-4 py-2 text-gray-700">{{ list.client }}</td>
           <td class="px-4 py-2 text-gray-700">{{ list.title }}</td>
           <td class="px-4 py-2 text-gray-700">{{ list.date }}</td>
           <td class="px-4 py-2">
